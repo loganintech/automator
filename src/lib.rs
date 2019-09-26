85578a2206eadac6abf5af97a8bbb5ffd4ba27fa
Logan Saso <logansaso@gmail.com>
2019-09-25 23:48:17 -0700

    some changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,32 @@
-pub trait Event {
-    fn id() -> &'static str;
-}
-
-pub trait Provider {
-    fn poll(&self) -> Option<String>;
-}
+use std::collections::HashMap;
+use std::rc::{Rc, Weak};
 
 pub trait Action {
+    fn key(&self) -> &str;
     fn trigger(&mut self, data: String) -> bool;
 }
 
 #[derive(Default)]
-pub struct Connector {
-    connections: Vec<(Box<dyn Provider>, Box<dyn Action>)>,
+pub struct Actions<'a> {
+    actions: HashMap<&'a str, Rc<dyn Action>>,
 }
 
-impl Connector {
+impl<'a> Actions<'a> {
     pub fn new() -> Self {
         Default::default()
     }
 
-    pub fn with_connection(mut self, con: (Box<dyn Provider>, Box<dyn Action>)) -> Self {
-        self.connections.push(con);
+    pub fn with_action<T: Action + 'a>(&mut self, action: T) -> &mut Self {
+        let act = Rc::new(action);
+        let weak = act.clone();
+        let weak = Rc::downgrade(&weak);
+        self.actions.insert(weak.key(), act);
         self
     }
 
     pub fn run(&mut self) -> std::result::Result<(), &'static str> {
-        loop {
-            for (prov, act) in self.connections.iter_mut() {
-                if let Some(data) = prov.poll() {
-                    act.trigger(data);
-                }
-            }
-        }
         Ok(())
     }
 }
+
+
