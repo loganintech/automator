223cc0a112c08f511a571cbc06503aff9a5f613b
Logan Saso <logansaso@gmail.com>
2020-03-22 01:43:02 -0700

    added interval

---
+++ src/interval.rs
@@ -0,0 +1,14 @@
+use std::time::{Duration, Instant};
+
+struct Interval {
+    d: Duration,
+    prev: Instant,
+}
+
+impl Interval {
+    fn with_duration(d: Duration) -> Self {
+        Interval {
+            d:
+        }
+    }
+}
