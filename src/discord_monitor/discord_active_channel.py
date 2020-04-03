54b041497b08276e5c1a22278843a198fa7dc5d6
Logan Saso <logansaso@gmail.com>
2020-04-02 23:54:48 -0700

    Added some better python error handlign

--- src/discord_monitor/discord_active_channel.py
+++ src/discord_monitor/discord_active_channel.py
@@ -4,12 +4,16 @@ import discord
 import asyncio
 from pathlib import Path
 import os
+import setproctitle
+import sys
+
+setproctitle.setproctitle("automator-discord_active_channel")
 
 
 class DClient(discord.Client):
 
     currchannel = ""
-    status = False
+    is_in_channel = False
     filepath = os.path.join(Path.home(), ".is_in_discord")
 
     async def on_message(self, message):
@@ -21,18 +25,31 @@ class DClient(discord.Client):
                     continue
                 members = channel.members
                 if "JewsOfHazard#0001" in [str(member) for member in members]:
-                    if not self.status or self.currchannel != channel:
+                    if not self.is_in_channel or self.currchannel != channel:
                         with open(self.filepath, "w+") as f:
                             f.write(str(channel) + "\n")
-                    self.status = True
+                    self.is_in_channel = True
                     return
 
         with open(self.filepath, "w+") as f:
             f.write("")
-        self.status = False
+        self.is_in_channel = False
+
+    async def on_error(self, _args, _kwargs):
+        pass
 
 
-client = DClient()
-with open("token.txt", "r") as f:
-    token = f.readline()
-    client.run(token, bot=False)
+async def main():
+
+    while True:
+        try:
+            client = DClient()
+            with open("token.txt", "r") as f:
+                token = f.readline()
+                await client.login(token, bot=False)
+                await client.connect(reconnect=False)
+        except Exception as e:
+            print("Error occurred running the bot:", e, file=sys.stderr)
+
+if __name__ == "__main__":
+    asyncio.run(main())
